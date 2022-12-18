af6ad1faed0581270eb9acd9e375aa3acb1c54b6
DenisKolmykov <soary@yandex.ru>
2022-12-18 11:39:42 +0300

    add Less2_LearnC#_Seminar_LastDigit

---
+++ Less2_LearnC#_Seminar_LastDigit/Program.cs
@@ -0,0 +1,23 @@
+﻿// 2. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+
+int GetLastDigit(int num)
+{
+    return num % 10;
+}
+
+int num;
+int lastDigit;
+
+Console.Clear();
+Console.WriteLine("Программа показывает последнюю цифру трехзначного числа. ");
+Console.WriteLine();
+Console.Write("Введите трехзначное число: ");
+num = int.Parse(Console.ReadLine()!);
+if (num < 1000 & num > 99)
+{
+    lastDigit = GetLastDigit(num);
+    Console.WriteLine($"Последняя цифра от введенного числа {num} будет = {lastDigit}.");
+}
+else Console.WriteLine("Вы ввели не трехзначное число");
