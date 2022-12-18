7cf9b8b5ed67171b1752a0ad7292a2729a7042b7
DenisKolmykov <soary@yandex.ru>
2022-12-18 12:00:33 +0300

    add Less2_learnC#_Seminar_02

---
+++ Less2_learnC#_Seminar_02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа выводит случайное число и находит наибольшую цифру в этом числе
+
+int GetMaxDigit(int number)
+{
+    int maxDigit;
+    int secondDigit = number % 10;
+    int ferstDigit = number / 10;
+    if (ferstDigit > secondDigit)
+    {
+        maxDigit = ferstDigit;
+    }
+    else maxDigit = secondDigit;
+    return maxDigit;
+}
+
+int num;
+int maxDigit;
+num = new Random().Next(10, 100);
+Console.WriteLine ($"Будем искать наибольшую цифру в числе {num}");
+maxDigit = GetMaxDigit (num);
+Console.WriteLine ($"Наибольшая цифра в числе {num} будет = {maxDigit}");
