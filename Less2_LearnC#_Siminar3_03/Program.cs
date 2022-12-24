a54cb70a40affa8153161b0e7bbf210108b202ce
DenisKolmykov <soary@yandex.ru>
2022-12-24 11:52:26 +0300

    add Less2_LearnC#_Siminar3_03

---
+++ Less2_LearnC#_Siminar3_03/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа выдает таблицу квадратов чисел от 1 до N
+
+void PrintTableSqrt(int n)
+{
+    int i = 1;
+    while (i <= n)
+    {
+        Console.WriteLine(i*i);
+        i++;
+    }
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.WriteLine("Таблица квадратов чисел от 1 до N");
+PrintTableSqrt(number);
