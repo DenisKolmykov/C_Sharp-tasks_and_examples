79073c96cbc4517f1993df6ebf386ed82687a6fc
DenisKolmykov <soary@yandex.ru>
2022-12-24 13:04:47 +0300

    add Less2_LearnC#_HW3_task23 (таблица кубов от 1 до N)

---
+++ Less2_LearnC#_HW3_task23/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N
+
+void PrintCubeTableOfNumber(int num)
+{
+    int i = 1;
+    if (num == 0)
+    {
+        Console.Write("Вы ввели значение `ноль`, таблица кубов состоит из одного значения `ноль`");
+        Console.WriteLine();
+    }
+    else
+    {
+        while (i <= Math.Abs(num)) // Abs добавлено для возможности работы с отрицательными числами
+        {
+            if (num > 0)
+            {
+                Console.Write($"{i * i * i}, ");
+            }
+            else // для отрицательных чисел
+            {
+                Console.Write($"{-i * i * i}, ");
+            }
+            i++;
+        }
+        Console.WriteLine(); // если эту пустую строку не ставить, то в конце таблицы прога выдает символ "%"
+    }
+
+}
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+
+Console.WriteLine($"Таблица кубов чисел от +/-1 до {number}:");
+PrintCubeTableOfNumber(number);
+
+Console.WriteLine();
