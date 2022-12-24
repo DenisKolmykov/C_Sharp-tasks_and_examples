297953bb154180f6446436e298fb4564652b5101
DenisKolmykov <soary@yandex.ru>
2022-12-24 11:50:34 +0300

    add Less2_LearnC#_Seminar3_02 lite edit

--- Less2_LearnC#_Seminar3_02/Program.cs
+++ Less2_LearnC#_Seminar3_02/Program.cs
@@ -1,12 +1,12 @@
 ﻿//  программа определяет расстояние между точками (координаты XY каждой точки) на 2D плоскости
 
-void PrintDistanceT1T2(int x1, int y1, int x2, int y2)
+void PrintDistanceT1T2(double x1, double y1, double x2, double y2)
 {
     double distance = Math.Sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
-    Console.WriteLine($"Расстояние между точкой Т1 ({x1}, {y1}) и точкой Т2 ({x2}, {y2}) = {distance}");
+    Console.WriteLine($"Расстояние между точкой Т1 ({x1}, {y1}) и точкой Т2 ({x2}, {y2}) = {distance:f}");
 }
 
-int x1 = 7, y1 = -5;
-int x2 = 1, y2 = -1;
+double x1 = 7, y1 = -5;
+double x2 = 1, y2 = -1;
 
 PrintDistanceT1T2(x1, y1, x2, y2);
