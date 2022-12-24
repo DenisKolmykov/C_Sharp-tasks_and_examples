6df3cc850ca1577b92c47ffb1ff80ac3624b4c5c
DenisKolmykov <soary@yandex.ru>
2022-12-24 11:51:58 +0300

    add Less2_LearnC#_Siminar3_01

---
+++ Less2_LearnC#_Siminar3_01/Program.cs
@@ -0,0 +1,38 @@
+﻿// при указании номера четверти
+// программа выводит диапазон возможных значений координат 
+
+void PrintXYFromQuarter(int q)
+{
+    if (q == 1)
+    {
+        // int x = new Random().Next(1, int.MaxValue);
+        // int y = new Random().Next(1, int.MaxValue);
+        Console.WriteLine($"Диапазон возможных значенией при X > 0 и У > 0 ");
+    }
+    else if (q == 2)
+    {
+        // int x = new Random().Next(1, int.MaxValue);
+        // int y = new Random().Next(-1, int.MinValue);
+        Console.WriteLine($"Диапазон возможных значенией при X > 0 и У < 0 ");
+    }
+
+    else if (q == 3)
+    {
+        //int x = new Random().Next(-1, int.MinValue);
+        //int y = new Random().Next(-1, int.MinValue);
+        Console.WriteLine($"Диапазон возможных значенией при X < 0 и У < 0 ");
+    }
+    else if (q == 4)
+    {
+        // int x = new Random().Next(-1, int.MaxValue);
+        // int y = new Random().Next(1, int.MaxValue);
+        Console.WriteLine($"Диапазон возможных значенией при X < 0 и У > 0 ");
+    }
+}
+    // int quarter = 1;
+    
+    Console.Clear();
+    
+    Console.Write("Введите номер четверти от 1 до 4: ");
+    int quarter = int.Parse(Console.ReadLine()!);
+    PrintXYFromQuarter(quarter);
