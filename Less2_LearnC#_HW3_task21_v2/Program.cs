ffa099b9470d507337ae45d7dbf87a7e5a45cd96
DenisKolmykov <soary@yandex.ru>
2022-12-25 21:25:49 +0300

    add Less2_LearnC#_HW3_task21_v2 (ввод координат в строку через пробел)

---
+++ Less2_LearnC#_HW3_task21_v2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+void PrintDistanceT1T2(double[] arrT1, double[] arrT2)
+{
+    //while (int i<3)
+    {
+
+    }
+    double distance = Math.Sqrt((arrT2[0] - arrT1[0]) * (arrT2[0] - arrT1[0]) + (arrT2[1] - arrT1[1]) * (arrT2[1] - arrT1[1]) + (arrT2[2] - arrT1[2]) * (arrT2[2] - arrT1[2]));
+    Console.WriteLine($"Расстояние между точками Т1 ({arrT1[0]}; {arrT1[1]}; {arrT1[2]}) и Т2 ({arrT2[0]}; {arrT2[1]}; {arrT2[2]})");
+    Console.WriteLine($"составляет {distance:f}");
+}
+
+Console.Clear();
+
+Console.WriteLine("Введите черрез пробел координаты точки Т1 (x,y,z): ");
+var CoordinatesOfPointT1 = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+Console.WriteLine();
+
+Console.WriteLine("Введите через пробел координаты точки Т2 (x,y,z): ");
+var CoordinatesOfPointT2 = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+//Console.WriteLine($"координаты:" + CoordinatesOfPoint[0] + "; " + CoordinatesOfPoint[1] + "; " + CoordinatesOfPoint[2]);
+
+Console.WriteLine();
+
+PrintDistanceT1T2(CoordinatesOfPointT1,CoordinatesOfPointT2);
+
+Console.WriteLine();
