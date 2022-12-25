14b28d2c0d99fc85a9b989acd10f44185a1e210b
DenisKolmykov <soary@yandex.ru>
2022-12-25 20:18:00 +0300

    add Less2_LearnC#_HW3_task19 (палиндром)

---
+++ Less2_LearnC#_HW3_task19/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+void PrintPolindrom(int num)
+{
+    int i = 0;
+
+    string strNum = num.ToString();
+    int length = strNum.Length;
+    
+    char[] reverseCharNum = new char[length];
+
+    while (i < length)
+    {
+        reverseCharNum[i] = strNum[length - i - 1];
+        i++;
+    }
+
+    string reverseStrNum = new string(reverseCharNum);
+
+    int reverseNum = Int32.Parse(reverseStrNum);
+
+    Console.WriteLine();
+    Console.WriteLine($"Обратная запись числа: {reverseNum}");
+    
+        if (num == reverseNum)
+        {
+            Console.WriteLine("Введенное Вами число ЯВЛЯЕТСЯ палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Введенное Вами число НЕ является палиндромом.");
+        }
+}
+
+Console.Clear();
+
+Console.Write("Введите любое целое число (int): ");
+int number = int.Parse(Console.ReadLine()!);
+
+PrintPolindrom(number);
+
+Console.WriteLine();
