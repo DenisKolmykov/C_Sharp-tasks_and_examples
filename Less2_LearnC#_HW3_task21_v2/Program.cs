561b5986a5a277cf9d16ddabff29828563c07f08
DenisKolmykov <soary@yandex.ru>
2022-12-25 22:26:34 +0300

    add Less2_LearnC#_HW3_task21_v2 (ввод координат в одну строку через пробел)

--- Less2_LearnC#_HW3_task21_v2/Program.cs
+++ Less2_LearnC#_HW3_task21_v2/Program.cs
@@ -4,10 +4,6 @@
 
 void PrintDistanceT1T2(double[] arrT1, double[] arrT2)
 {
-    //while (int i<3)
-    {
-
-    }
     double distance = Math.Sqrt((arrT2[0] - arrT1[0]) * (arrT2[0] - arrT1[0]) + (arrT2[1] - arrT1[1]) * (arrT2[1] - arrT1[1]) + (arrT2[2] - arrT1[2]) * (arrT2[2] - arrT1[2]));
     Console.WriteLine($"Расстояние между точками Т1 ({arrT1[0]}; {arrT1[1]}; {arrT1[2]}) и Т2 ({arrT2[0]}; {arrT2[1]}; {arrT2[2]})");
     Console.WriteLine($"составляет {distance:f}");
@@ -22,8 +18,6 @@ Console.WriteLine();
 
 Console.WriteLine("Введите через пробел координаты точки Т2 (x,y,z): ");
 var CoordinatesOfPointT2 = Console.ReadLine().Split().Select(double.Parse).ToArray();
-
-//Console.WriteLine($"координаты:" + CoordinatesOfPoint[0] + "; " + CoordinatesOfPoint[1] + "; " + CoordinatesOfPoint[2]);
 
 Console.WriteLine();
 
