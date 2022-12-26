7d875bcb11cfab5b21e815b7ef3d90a181f5b6e1
DenisKolmykov <soary@yandex.ru>
2022-12-26 08:57:17 +0300

    add Less2_LearnC#_HW3_task19 (палиндром: через string)

--- Less2_LearnC#_HW3_task19/Program.cs
+++ Less2_LearnC#_HW3_task19/Program.cs
@@ -2,29 +2,29 @@
 // Напишите программу, которая принимает на вход пятизначное число 
 // и проверяет, является ли оно палиндромом.
 
-void PrintPolindrom(int num)
+void PrintPlindrom(int num)
 {
     int i = 0;
 
-    string strNum = num.ToString();
-    int length = strNum.Length;
+    string strNum = num.ToString(); // введенное пользователем число преобразуем в string
+    int length = strNum.Length; // определяем длину строки (по-сути - кол-во разрядов числа num)
     
-    char[] reverseCharNum = new char[length];
+    char[] reverseCharNum = new char[length]; // сюда записываем каждую отдельную цифру разряда числа в обратном порядке
 
     while (i < length)
     {
-        reverseCharNum[i] = strNum[length - i - 1];
+        reverseCharNum[i] = strNum[length - i - 1]; // цифра с последнего места [последний индекс] - записывается на первое место [0]  и тд
         i++;
     }
 
-    string reverseStrNum = new string(reverseCharNum);
+    string reverseStrNum = new string(reverseCharNum); // преобразовываем массив с отдельными элементами - в строку
 
-    int reverseNum = Int32.Parse(reverseStrNum);
+    int reverseNum = Int32.Parse(reverseStrNum); // преобразовываем строку - в число
 
     Console.WriteLine();
     Console.WriteLine($"Обратная запись числа: {reverseNum}");
     
-        if (num == reverseNum)
+        if (num == reverseNum) // сравниваем введенное число num с его обратной записью
         {
             Console.WriteLine("Введенное Вами число ЯВЛЯЕТСЯ палиндромом.");
         }
@@ -39,6 +39,6 @@ Console.Clear();
 Console.Write("Введите любое целое число (int): ");
 int number = int.Parse(Console.ReadLine()!);
 
-PrintPolindrom(number);
+PrintPalindrom(number);
 
 Console.WriteLine();
