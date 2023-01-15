bc24bb4819e54df6d051d0b131b53bf7f2461d33
DenisKolmykov <soary@yandex.ru>
2023-01-15 20:25:15 +0300

    add Less3_LearnC#_Seminar_5_33_FindNum (находим заданное число в массиве)

---
+++ Less3_LearnC#_Seminar_5_33_FindNum/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 33: Задайте массив. 
+// Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, -3] -> нет
+// -3; массив [6, 7, 19, 345, -3] -> да
+
+
+int[] CreateArray(int length = 10, int minElem = -100, int maxElem = 100)
+{
+    int[] arr = new int[length];
+    var rand = new Random();
+
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rand.Next(minElem, maxElem + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+void FindNumInArray(int[] arr, int number)
+{
+    int i = 0;
+    while (i < arr.Length)
+    {
+        if (number == arr[i])
+        {
+            Console.WriteLine($"Указанное число `{number}` находится в маcсиве на позиции `{i + 1}`");
+            goto end;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    Console.WriteLine($"В массиве отсутствует указанное число `{number}` ");
+    
+    end:;
+}
+
+
+Console.Clear();
+
+int[] array = CreateArray();
+Console.WriteLine("Создан массив: ");
+PrintArray(array);
+
+Console.WriteLine();
+
+Console.Write($"Введите число для поиска в массиве: ");
+int num = int.Parse(Console.ReadLine()!);
+
+FindNumInArray(array, num);
+
+Console.WriteLine();
