c4c343fda6bb61fae742f1fc1e101155bfd3a523
DenisKolmykov <soary@yandex.ru>
2023-01-22 01:41:50 +0300

    add Less4_LearnC#_Seminar6_45_CopyArray (поэлементное копирование массива)

---
+++ Less4_LearnC#_Seminar6_45_CopyArray/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 45
+// напишите программу, которая будет создавать копию заданного массива
+// с помощью поэлементного копирования
+
+
+double[] CreateArray(int length = 10, int minElem = -100, int maxElem = 100)
+{
+    double[] arr = new double[length];
+    var rand = new Random();
+
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rand.Next(minElem, maxElem + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+double[] GetCopyArray(double[] arr)
+{
+    double[] copyarr = new double[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        copyarr[i] = arr[i];
+    }
+    return copyarr;
+}
+
+Console.Clear();
+
+double[] array = CreateArray();
+Console.WriteLine("Создан массив: ");
+PrintArray(array);
+
+Console.WriteLine();
+
+double[] copyArray = GetCopyArray(array);
+Console.WriteLine("Копия исходного массива (поэлементное копирование): ");
+PrintArray(copyArray);
+
+Console.WriteLine();
