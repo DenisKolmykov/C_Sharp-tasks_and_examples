cedd7fd06894acfb1b41f45ba942dc79affef7a6
DenisKolmykov <soary@yandex.ru>
2023-01-21 15:44:34 +0300

    add HW6_task41_CountNums_v1_arr (ввод чисел через пробел)

---
+++ HW6_task41_CountNums_v1_arr/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int GetCountInterNumbers(double[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+Console.Clear();
+
+Console.WriteLine("Введите через пробел любое количество любых чисел: ");
+Console.Write(": ");
+double[] numbers = Console.ReadLine()!.Split().Select(double.Parse).ToArray();
+
+Console.WriteLine();
+int count = GetCountInterNumbers(numbers);
+Console.WriteLine("Среди указанных чисел, " + count + " - больше нуля.");
+
+Console.WriteLine();
