94e27c45b0a774da45b0914a2f3d6e9cd7af5484
DenisKolmykov <soary@yandex.ru>
2023-01-21 15:45:16 +0300

    add HW6_task41_CountNums_v2_inter (ввод заданного кол-ва чисел в отдельном методе)

---
+++ HW6_task41_CountNums_v2_inter/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+double[] InterNumbers(int m)
+{
+    double[] arr = new double[m];
+
+    Console.WriteLine("Введите " + m + " любых чисел:");
+
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write((i + 1) + "-е число: ");
+        arr[i] = double.Parse(Console.ReadLine()!);
+    }
+    return arr;
+}
+
+int GetCountInterNumbers(double[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+int m = 5;
+
+Console.Clear();
+
+double[] numbersForCount = InterNumbers(m);
+
+int count = GetCountInterNumbers(numbersForCount);
+Console.WriteLine("Среди указанных чисел, " + count + " - больше нуля.");
+
+Console.WriteLine();
