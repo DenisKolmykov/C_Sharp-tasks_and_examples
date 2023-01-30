47b066a72060918fce26df3976256c6d687878c4
DenisKolmykov <soary@yandex.ru>
2023-01-30 16:05:57 +0300

    add HW9_task66_Recurcion_Sum_From_M_To_N (Рекурсия Сумма чисел от М до N)

---
+++ HW9_task66_Recurcion_Sum_From_M_To_N/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 66: 
+Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetSumFromMToN(int m, int n)
+{
+    if (n == m)
+    {
+        return m;
+    }
+    int sum = n + GetSumFromMToN(m, n-1);
+    return sum;
+}
+
+
+int m = 1;
+int n = 15;
+
+Console.WriteLine(GetSumFromMToN(m, n));
